3f358f1d37f071f317b15259ebffacab451deaf6
Mr.Tang <1084491297@qq.com>
2025-03-24 08:45:00 +0800

    refactor(connectkit): 使用环境变量配置 Particle Connect
    - 将 projectId、clientKey 和 appId 替换为环境变量
    - 将 authTypes 替换为环境变量
    - 这样做可以提高配置的灵活性和安全性

--- src/components/connectkit.tsx
+++ src/components/connectkit.tsx
@@ -11,9 +11,9 @@ import { solanaWalletConnectors,injected as solaInjected } from '@particle-netwo
 
 
 const config = createConfig({
-  projectId: '43fdacab-dacb-4d94-9026-fd32573c1980',
-  clientKey: 'cNFmDTYTb0JiorIBCyApd7G9kuFHpmGOBx37m2W6',
-  appId: '1a8c4140-b087-44ff-89de-031cb40c2bf7',
+  projectId: import.meta.env.projectId,
+  clientKey: import.meta.env.clientKey,
+  appId: import.meta.env.appId,
   appearance: {"recommendedWallets":[{"walletId":"phantom","label":"none"}],"theme":{"--pcm-font-family":"'__Poppins_68bcaa', '__Poppins_Fallback_68bcaa'","--pcm-body-background":"#ffffff","--pcm-body-background-secondary":"#EFF0F2","--pcm-body-background-tertiary":"#F9F9FA","--pcm-body-color":"#181B1E","--pcm-body-color-secondary":"#8B8EA1","--pcm-body-color-tertiary":"#DCDFE6","--pcm-primary-button-bankground":"#181B1E","--pcm-primary-button-color":"#ffffff","--pcm-primary-button-hover-background":"#353738","--pcm-secondary-button-color":"#181B1E","--pcm-secondary-button-bankground":"#EFF0F2","--pcm-secondary-button-hover-background":"#F0F6FF","--pcm-body-action-color":"#999999","--pcm-button-border-color":"#F5F6FB","--pcm-accent-color":"#4f46e5","--pcm-rounded-sm":"4px","--pcm-rounded-md":"8px","--pcm-rounded-lg":"11px","--pcm-rounded-xl":"22px","--pcm-button-font-weight":"500","--pcm-modal-box-shadow":"0px 2px 4px rgba(0, 0, 0, 0.1)"},"splitEmailAndPhone":false,"collapseWalletList":false,"hideContinueButton":false,"connectorsOrder":["email","social","wallet"],"language":"en-US","collapsePasskeyButton":true},
   walletConnectors: [
     
@@ -24,11 +24,7 @@ const config = createConfig({
     }),
     
     authWalletConnectors({
-      authTypes: [
-        "google","twitter" ,
-        
-        "email",
-      ],
+      authTypes: import.meta.env.authTypes,
       fiatCoin: "USD",
       promptSettingConfig:{
       promptMasterPasswordSettingWhenLogin:1,
