91ccaf7b337e8874bcb58f9328f818e6a6572d3b
Mr.Tang <1084491297@qq.com>
2025-03-24 08:53:51 +0800

    build(env): 更新环境变量前缀并调整配置使用
    - 在 .env.development 和 .env.production 文件中，将环境变量名前缀改为 VITE_
    - 在 connectkit.tsx 中，更新环境变量引用，使用新的 VITE_ 前缀
    - 调整 authTypes 配置，使用 JSON.parse 解析环境变量中的数组

--- src/components/connectkit.tsx
+++ src/components/connectkit.tsx
@@ -11,9 +11,9 @@ import { solanaWalletConnectors,injected as solaInjected } from '@particle-netwo
 
 
 const config = createConfig({
-  projectId: import.meta.env.projectId,
-  clientKey: import.meta.env.clientKey,
-  appId: import.meta.env.appId,
+  projectId: import.meta.env.VITE_PROJECTID,
+  clientKey: import.meta.env.VITE_CLIENTKEY,
+  appId: import.meta.env.VITE_APPID,
   appearance: {"recommendedWallets":[{"walletId":"phantom","label":"none"}],"theme":{"--pcm-font-family":"'__Poppins_68bcaa', '__Poppins_Fallback_68bcaa'","--pcm-body-background":"#ffffff","--pcm-body-background-secondary":"#EFF0F2","--pcm-body-background-tertiary":"#F9F9FA","--pcm-body-color":"#181B1E","--pcm-body-color-secondary":"#8B8EA1","--pcm-body-color-tertiary":"#DCDFE6","--pcm-primary-button-bankground":"#181B1E","--pcm-primary-button-color":"#ffffff","--pcm-primary-button-hover-background":"#353738","--pcm-secondary-button-color":"#181B1E","--pcm-secondary-button-bankground":"#EFF0F2","--pcm-secondary-button-hover-background":"#F0F6FF","--pcm-body-action-color":"#999999","--pcm-button-border-color":"#F5F6FB","--pcm-accent-color":"#4f46e5","--pcm-rounded-sm":"4px","--pcm-rounded-md":"8px","--pcm-rounded-lg":"11px","--pcm-rounded-xl":"22px","--pcm-button-font-weight":"500","--pcm-modal-box-shadow":"0px 2px 4px rgba(0, 0, 0, 0.1)"},"splitEmailAndPhone":false,"collapseWalletList":false,"hideContinueButton":false,"connectorsOrder":["email","social","wallet"],"language":"en-US","collapsePasskeyButton":true},
   walletConnectors: [
     
@@ -24,7 +24,7 @@ const config = createConfig({
     }),
     
     authWalletConnectors({
-      authTypes: import.meta.env.authTypes,
+      authTypes: JSON.parse(import.meta.env.VITE_AUTHTYPES),
       fiatCoin: "USD",
       promptSettingConfig:{
       promptMasterPasswordSettingWhenLogin:1,
