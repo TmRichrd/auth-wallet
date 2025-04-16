bc7d9a9568924a37363362713b43497a90ae4273
Mr.Tang <1084491297@qq.com>
2025-04-16 16:04:57 +0800

    feat(components): 更新 connectkit 组件的外观和钱包连接器配置
    - 修改了 connectkit 组件的外观设置，更新了推荐钱包列表
    - 新增了多个钱包连接器，包括 MetaMask、OKX Wallet 和 Trust Wallet
    - 移除了 Solana 钱包连接器的配置
    - 调整了连接器的显示顺序和连接器函数的配置

--- src/components/connectkit.tsx
+++ src/components/connectkit.tsx
@@ -6,7 +6,7 @@ import { ConnectKitProvider, createConfig } from '@particle-network/connectkit';
 import { wallet, type EntryPosition } from '@particle-network/connectkit/wallet';
 import { solana, mainnet, base, arbitrum, avalanche, linea, bsc, optimism, polygon, arbitrumNova } from '@particle-network/connectkit/chains';
 import { authWalletConnectors } from '@particle-network/connectkit/auth';
-import { evmWalletConnectors, coinbaseWallet, injected } from '@particle-network/connectkit/evm';
+import { evmWalletConnectors, coinbaseWallet, injected, walletConnect } from '@particle-network/connectkit/evm';
 import { solanaWalletConnectors,injected as solaInjected } from '@particle-network/connectkit/solana';
 
 
@@ -14,12 +14,14 @@ const config = createConfig({
   projectId: import.meta.env.VITE_PROJECTID,
   clientKey: import.meta.env.VITE_CLIENTKEY,
   appId: import.meta.env.VITE_APPID,
-  appearance: {"recommendedWallets":[{"walletId":"phantom","label":"none"}],"theme":{"--pcm-font-family":"'__Poppins_68bcaa', '__Poppins_Fallback_68bcaa'","--pcm-body-background":"#ffffff","--pcm-body-background-secondary":"#EFF0F2","--pcm-body-background-tertiary":"#F9F9FA","--pcm-body-color":"#181B1E","--pcm-body-color-secondary":"#8B8EA1","--pcm-body-color-tertiary":"#DCDFE6","--pcm-primary-button-bankground":"#181B1E","--pcm-primary-button-color":"#ffffff","--pcm-primary-button-hover-background":"#353738","--pcm-secondary-button-color":"#181B1E","--pcm-secondary-button-bankground":"#EFF0F2","--pcm-secondary-button-hover-background":"#F0F6FF","--pcm-body-action-color":"#999999","--pcm-button-border-color":"#F5F6FB","--pcm-accent-color":"#4f46e5","--pcm-rounded-sm":"4px","--pcm-rounded-md":"8px","--pcm-rounded-lg":"11px","--pcm-rounded-xl":"22px","--pcm-button-font-weight":"500","--pcm-modal-box-shadow":"0px 2px 4px rgba(0, 0, 0, 0.1)"},"splitEmailAndPhone":false,"collapseWalletList":false,"hideContinueButton":false,"connectorsOrder":["email","social","wallet"],"language":"en-US","collapsePasskeyButton":true},
+  appearance: {"recommendedWallets":[{"walletId":"metaMask","label":"Recommended"},{"walletId":"coinbaseWallet","label":"Popular"},{"walletId":"okxWallet","label":"none"},{"walletId":"phantom","label":"none"},{"walletId":"trustWallet","label":"none"},{"walletId":"bitKeep","label":"none"},{"walletId":"walletConnect","label":"none"}],"theme":{"--pcm-font-family":"'__Poppins_68bcaa', '__Poppins_Fallback_68bcaa'","--pcm-rounded-sm":"4px","--pcm-rounded-md":"8px","--pcm-rounded-lg":"11px","--pcm-rounded-xl":"22px"},"splitEmailAndPhone":false,"collapseWalletList":false,"hideContinueButton":false,"connectorsOrder":["email","wallet"],"language":"en-US","collapsePasskeyButton":true},
   walletConnectors: [
     
     evmWalletConnectors({
       metadata: { name: 'My App' },
-      connectorFns: [injected({ target: "phantom" })],
+      connectorFns: [injected({ target: "metaMask" }),injected({ target: "okxWallet" }),injected({ target: "trustWallet" }),injected({ target: "bitKeep" }),walletConnect({
+        showQrModal: false,
+      }),coinbaseWallet()],
       multiInjectedProviderDiscovery: true,
     }),
     
@@ -30,10 +32,6 @@ const config = createConfig({
       promptMasterPasswordSettingWhenLogin:1,
       promptPaymentPasswordSettingWhenSign:1,
       }
-    }),
-    
-    solanaWalletConnectors({
-      connectorFns: [solaInjected({ target: "phantom" })],
     }),
   ],
   plugins: [
